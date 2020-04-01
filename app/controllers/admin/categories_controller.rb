d2feff8c4d502b11c9696fdd6c4e92670707bdb5
Eric-Lombardo <ejlanim8or@gmail.com>
2020-04-01 19:11:57 +0000

    display all category names in a table

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,35 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_AUTH_NAME'], password: ENV['HTTP_BASIC_AUTH_PASSWORD']
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to [:admin, :products], notice: 'Product created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :description,
+      :category_id,
+      :quantity,
+      :image,
+      :price
+    )
+  end
+
 end

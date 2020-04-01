cdcae265da77e189396801e8e38371767b0c6492
Eric-Lombardo <ejlanim8or@gmail.com>
2020-04-01 18:19:53 +0000

    create about controller with CSS styling for /about

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  get 'about/', to: 'about#show'
 
   resource :cart, only: [:show] do
     post   :add_item

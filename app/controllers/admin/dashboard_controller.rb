ce690619c4b8b913e6d0714537e23b430578587e
Eric-Lombardo <ejlanim8or@gmail.com>
2020-04-01 13:44:13 +0000

    fetch DB data and store in controller variable

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['HTTP_BASIC_AUTH_NAME'], password: ENV['HTTP_BASIC_AUTH_PASSWORD']
 
   def show
+    @hello = 'hello'
+    @products = Product.count
+    @categories = Category.count
   end
 end

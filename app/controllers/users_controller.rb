348e8487e75452cc97bbe2b65a966361e0ad2974
Eric-Lombardo <ejlanim8or@gmail.com>
2020-04-02 15:30:11 +0000

    add logic to allow unique emails for sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    if user.save
+    if !User.where("email = ?", user_params["email"]).any? && user.save 
       session[:user_id] = user.id
       redirect_to '/'
     else

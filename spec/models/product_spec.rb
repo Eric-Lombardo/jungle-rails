6001a5e52c05f0ecbd5f0105e011f50bcdd39e6b
Eric-Lombardo <ejlanim8or@gmail.com>
2020-04-06 19:48:20 +0000

    create and pass 5 tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe Product, type: :model do
       @category = Category.find_or_create_by! name: 'Motorcycle'
       @product = @category.products.create!({
         name:  'Ducati Streetfighter 848',
-        description: "My dream motorcycle",
         quantity: 1,
         price: 16000
       })
@@ -17,11 +16,31 @@ RSpec.describe Product, type: :model do
     it 'is valid with valid attributes' do
       expect(@product).to be_valid
     end
+
+    it 'should not validate if name is nil' do
+      @product.name = nil
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages[:name]).to include("can't be blank")
+    end
+
+    it 'should not validate if quantity is nil' do
+      @product.quantity = nil
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages[:quantity]).to include("can't be blank")
+    end
+
+    it 'should not validate if price is nil' do 
+      @product.price_cents = nil
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages[:price]).to include("can't be blank")
+    end
+
+    it 'should not validate if category is nil' do
+      @product.category = nil
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages[:category]).to include("can't be blank")
+    end
+
   end
 end
 
-
-# validates :name, presence: true
-# validates :price, presence: true
-# validates :quantity, presence: true
-# validates :category, presence: true

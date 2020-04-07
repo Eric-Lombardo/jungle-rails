000b56733ff84e6d816fad05b83c5024e548eb86
Eric-Lombardo <ejlanim8or@gmail.com>
2020-04-07 18:06:32 +0000

    add product_detail_spec test

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        price: 64.99,
+        image: open_asset('apparel1.jpg')
+      )
+    end
+  end
+
+  scenario "They see product details of 1 specific product" do
+    # ACT
+    visit root_path
+    find('img', match: :first).click
+
+    # DEBUG
+    save_screenshot
+    
+    # VERIFY
+    expect(page).to have_css '.product-detail', count: 1 
+    save_screenshot
+  end
+
+end
